b7f8fe268481bef87266bdcb8aa22d54b6485e9d
Aayush-95 <aayushbhavsar.com@gmail.com>
2025-10-27 22:10:05 +0530

    Added Login Page

---
+++ frontend/src/LoginPage.jsx
@@ -0,0 +1,113 @@
+import { useState } from "react";
+
+function LoginPage() {
+    const [username, setUsername] = useState("");           /*variable for username*/
+    const [password, setPassword] = useState("");           /*variable for password*/
+    const [errors, setErrors] = useState({username:"", password:"", general:""});            /*variable to store errors*/
+    const usernamePattern = /^[a-zA-Z0-9]+$/;           /*aplhanumeric username pattern */
+    const validUser = {username:"25BCA001",password:"password123"};             /*temporary variable for valid user details */
+
+    const handleSubmit = (e) =>{                    /*function to handle errors on submit */
+        e.preventDefault();             /*Will stop page from reloading on submit */
+        let isValid = true;
+        const newErrors = {username:"", password:"", general:""}
+
+        /*=================Validations for Empty Fields============================= */
+        if(username.trim()==="" && password.trim()===""){
+            newErrors.general = "Please fill Username and Password.";
+            isValid=false;
+        }
+        else if(username.trim()===""){
+            newErrors.username = "Please Enter your Username.";
+            isValid=false;
+        }
+        else if(password.trim()===""){
+            newErrors.password = "Please Enter your Password.";
+            isValid=false;
+        }
+
+        /*=================Validation for Alphanumeric Pattern Check============================= */
+        if(username && !usernamePattern.test(username)){
+            newErrors.username = "Username should only Contain Alphabets and Numbers.";
+            isValid=false;
+        }
+
+        /*=================Validation for Password Length Check============================= */
+        if(password && password.length<8){
+            newErrors.password = "Password must have at least 8 characters.";
+            isValid=false;
+        }
+
+        /*============if any validation fails from front-end side then it will return=====*/
+        if(!isValid){
+            setErrors(newErrors);
+            return;
+        }
+
+        /*Backend Validations to match correct username and password */
+        //calling API from backend (later)
+
+        if(username!==validUser.username || password!==validUser.password){     /*temporarily match username and pass with valid user */
+            newErrors.general = "Invalid Username or Password.";
+            setErrors(newErrors);
+            return;
+        }
+
+        /*if all validations pass then: */
+        setErrors({username: "", password: "", general: ""})
+        alert("Login Succesful!");
+    };
+
+    return (
+        <form id="loginForm" onSubmit={handleSubmit}>
+            {/*Main login container */}
+            <div className="loginContainer w-full bg-white border-gray-200 p-5 rounded-xl shadow-lg justify-center items-center justify-items-center">
+                {/* login header*/}
+                <div className="loginHeader justify-center items-center justify-items-center w-full m-2 mt-1">
+                    <p className="loginTitle p-1">College ERP Login</p>
+                    <p className="loginInstruction text-gray-500 p-1">
+                        Enter your credentials to access the system
+                    </p>
+                </div>
+                {/* login details container */}
+                <div className="loginDetails justify-start items-center w-full m-2">
+                    <p className="usernameLabel font-semibold text-sm my-1">Username</p>
+                    <input
+                        type="text"
+                        className="usernameInput border-none w-full rounded-lg p-2 bg-[#f5f5f4] placeholder-gray-500 my-1 focus:ring-2 focus:ring-blue-500 outline-none"
+                        placeholder="Enter Your Username"
+                        value={username}
+                        onChange={(e)=>setUsername(e.target.value)}
+                    />
+                    {errors.username && <p className="text-red-500 text-xs mt-1">{errors.username}</p>}  {/* only runs under condition */}
+
+                    <p className="passwordLabel font-semibold text-sm my-1">Password</p>
+                    <input
+                        type="password"
+                        className="passwordInput border-none w-full rounded-lg p-2 bg-[#f5f5f4] placeholder-gray-500 my-1 focus:ring-2 focus:ring-blue-500 outline-none"
+                        placeholder="Enter Your Password"
+                        value={password}
+                        onChange={(e)=>setPassword(e.target.value)}
+                    />
+                    {errors.password && <p className="text-red-500 text-xs mt-1">{errors.password}</p>} {/* only runs under condition */}
+
+                    {errors.general && <p className="text-red-500 text-sm mt-3 text-center">{errors.general}</p>} {/* only runs under condition */}
+                    <button
+                        type="submit"
+                        className="signInButton bg-blue-600 hover:bg-blue-700 text-white text-sm font-semibold w-full rounded-lg p-2 mt-6"
+                    >
+                        Sign In
+                    </button>
+                </div>
+                {/* forgot password conatiner */}
+                <div className="bottomDiv p-3">
+                    <a href="#" className="usernameLabel font-semibold text-sm my-1 text-blue-600 hover:text-blue-700" onClick={() => alert("Redirecting to password recovery page...")}>
+                        Forgot your Password?
+                    </a>
+                </div>
+            </div>
+        </form>
+    );
+}
+
+export default LoginPage;
