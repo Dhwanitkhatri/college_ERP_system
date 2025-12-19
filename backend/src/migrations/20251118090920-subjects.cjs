8bfe3609577b7ccd7269355dea1339c8589734bf
Yug Panchal <panchalyug80@gmail.com>
2025-12-19 19:53:02 +0530

    fix small error

--- backend/src/migrations/20251118090920-subjects.cjs
+++ backend/src/migrations/20251118090920-subjects.cjs
@@ -1,76 +1,78 @@
-'use strict';
+"use strict";
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-    async up(queryInterface, Sequelize) {
-        await queryInterface.createTable("Subjects", {
-            id: {
-                autoIncrement: true,
-                primaryKey: true,
-                type: Sequelize.INTEGER
-            },
-            subject_id: {
-                type: Sequelize.STRING,
-                unique: true,
-                allowNull: false
-            },
-            faculty_id: {
-                type: Sequelize.STRING,
-                allowNull: false,
-                references: {
-                    model: "Faculties",
-                    key: "faculty_id"
-                },
-                onUpdate: "CASCADE",
-                onDelete: "RESTRICT"
-            },
-            course_id: {
-                type: Sequelize.STRING,
-                allowNull: false,
-                references: {
-                    model: "Courses",
-                    key: "course_id"
-                },
-                onUpdate: "CASCADE",
-                onDelete: "RESTRICT"
-            },
-            subject_name: {
-                type: Sequelize.STRING,
-                allowNull: false,
-                unique: false
-            },
-            credit: {
-                type: Sequelize.INTEGER,
-                allowNull: false
-            },
-            semester: {
-                type: Sequelize.INTEGER,
-                allowNull: true,
-                validate: {
-                    min: 1, max: 6
-                }
-            },
-            lecture_per_week: {
-                type: Sequelize.INTEGER,
-                allowNull: true,
-                validate: {
-                    min: 1, max: 6
-                }
-            },createdAt: {
-              allowNull: false,
-              type: Sequelize.DATE,
-              defaultValue: Sequelize.literal('CURRENT_TIMESTAMP') // Optional but helpful
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Subjects", {
+      id: {
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      subject_id: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false,
+      },
+      faculty_id: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        references: {
+          model: "Faculties",
+          key: "faculty_id",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "RESTRICT",
+      },
+      course_id: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        references: {
+          model: "Courses",
+          key: "course_id",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "RESTRICT",
+      },
+      subject_name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: false,
+      },
+      credit: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      semester: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        validate: {
+          min: 1,
+          max: 6,
+        },
+      },
+      lecture_per_week: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        validate: {
+          min: 1,
+          max: 6,
+        },
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"), // Optional but helpful
       },
       updatedAt: {
-              allowNull: false,
-              type: Sequelize.DATE,
-              defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
-      }
-        }
-        );
-    },
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+    });
+  },
 
-    async down(queryInterface, Sequelize) {
-        await queryInterface.dropTable("Subjects");
-    }
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Subjects");
+  },
 };
