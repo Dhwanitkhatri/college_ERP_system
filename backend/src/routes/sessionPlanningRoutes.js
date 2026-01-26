abee631b972d3d05f7db570685ec6f61fb63d75f
Jiken-Patel <jikenpatel416@gmail.com>
2026-01-26 19:59:48 +0530

    Added Update and Delete Routes in Session-planning routes

--- backend/src/routes/sessionPlanningRoutes.js
+++ backend/src/routes/sessionPlanningRoutes.js
@@ -1,4 +1,4 @@
-import { createSessionPlan , getFacultyTechInfo , getAllSessionPlans , getFacultyScheduleForDate } from "../controllers/sessionPlanningController.js";
+import { createSessionPlan, getFacultyTechInfo, getAllSessionPlans, getFacultyScheduleForDate, updateSessionPlan, deleteSessionPlan } from "../controllers/sessionPlanningController.js";
 import { authMiddleware } from "../middleware/authMiddleware.js";
 import express from "express";
 
@@ -6,15 +6,21 @@ import express from "express";
 const router = express.Router();
 
 // Create session plan
-router.post('/',authMiddleware, createSessionPlan);
+router.post('/', authMiddleware, createSessionPlan);
 
 // Get faculty teaching information (classes and subjects taught)
-router.get('/teaching-info',authMiddleware, getFacultyTechInfo);
+router.get('/teaching-info', authMiddleware, getFacultyTechInfo);
 
 // Get all session plans for faculty
-router.get('/all',authMiddleware, getAllSessionPlans);
+router.get('/all', authMiddleware, getAllSessionPlans);
 
 // get session plan by date
-router.get('/date', authMiddleware, getFacultyScheduleForDate)
+router.get('/date', authMiddleware, getFacultyScheduleForDate);
+
+// Update session plan
+router.put('/:id', authMiddleware, updateSessionPlan);
+
+// Delete session plan
+router.delete('/:id', authMiddleware, deleteSessionPlan);
 
 export default router;
