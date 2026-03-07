40c5115aabbffb8c61133324cfddc8d2b7936e92
Jiken-Patel <jikenpatel416@gmail.com>
2026-03-07 14:49:55 +0530

    solve issues

--- backend/src/routes/learningMaterialRoutes.js
+++ backend/src/routes/learningMaterialRoutes.js
@@ -6,7 +6,7 @@ import {
     getMaterialById,
     downloadMaterial,
     deleteMaterial
-} from '../controllers/learningMaterial.controller.js';
+} from '../controllers/learningMaterialController.js';
 import { authMiddleware } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
